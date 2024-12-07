c00751c04cc31f5d7c57c83e4330d2653ae48e4c
havison <dieu2010@mail.ru>
2024-12-07 13:52:20 +1000

    v.2 (async)

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import asyncio
 import json
 import ssl
 import logging
+
+import requests
 import websockets
 from pybit.unified_trading import HTTP
 from config_data.config import Config, load_config
-from user import message_bybit_binance
+from user import message_bybit_binance, message_binance
 
 # Настройка логирования
 logger = logging.getLogger(__name__)
@@ -24,18 +26,14 @@ session = HTTP(
 
 data_bybit = session.get_tickers(category="linear")
 
-TOP_50_COINS = [
-    "BTCUSDT", "ETHUSDT", "BNBUSDT", "XRPUSDT", "ADAUSDT", "DOGEUSDT",
-    "SOLUSDT", "MATICUSDT", "DOTUSDT", "LTCUSDT", "SHIBUSDT", "TRXUSDT",
-    "AVAXUSDT", "UNIUSDT", "ATOMUSDT", "LINKUSDT", "XMRUSDT", "ETCUSDT",
-    "FILUSDT", "APEUSDT", "APTUSDT", "NEARUSDT", "ALGOUSDT", "QNTUSDT",
-    "VETUSDT", "ICPUSDT", "GRTUSDT", "EOSUSDT", "MANAUSDT", "SANDUSDT",
-    "AAVEUSDT", "KLAYUSDT", "XTZUSDT", "THETAUSDT", "AXSUSDT", "FTMUSDT",
-    "RUNEUSDT", "CHZUSDT", "CAKEUSDT", "LDOUSDT", "ZILUSDT", "ENJUSDT",
-    "1INCHUSDT", "IMXUSDT", "GALAUSDT", "CRVUSDT", "DYDXUSDT", "SUSHIUSDT"
-]
+TOP_50_COINS = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'ADAUSDT', 'DOGEUSDT', 'XRPUSDT', 'DOTUSDT', 'SOLUSDT', 'LINKUSDT', 'LTCUSDT', 'BCHUSDT', 'UNIUSDT', 'XLMUSDT', 'FILUSDT', 'THETAUSDT', 'VETUSDT', 'TRXUSDT', 'EOSUSDT', 'AAVEUSDT', 'XMRUSDT', 'ATOMUSDT', 'ALGOUSDT', 'CROUSDT', 'MKRUSDT', 'ETCUSDT', 'NEOUSDT', 'CAKEUSDT', 'COMPUSDT', 'MIOTAUSDT', 'DASHUSDT', 'KSMUSDT', 'ZECUSDT', 'XEMUSDT', 'XTZUSDT', 'BSVUSDT', 'AXSUSDT', 'WAVESUSDT', 'SUSHIUSDT', 'EGLDUSDT', 'MANAUSDT', 'HNTUSDT', 'HEDGUSDT', 'SHIBUSDT', 'CELUSDT', 'HTUSDT', 'ENJUSDT', 'GRTUSDT', 'ZRXUSDT', 'RENUSDT', 'BATUSDT']
 s = [dicts['symbol'] for dicts in data_bybit['result']['list']
      if 'USDT' in dicts['symbol'] and dicts['symbol'] not in TOP_50_COINS]
+
+url = 'https://fapi.binance.com/fapi/v2/ticker/price'
+response = requests.get(url)
+data_binance = response.json()
+binance_symbol = [data['symbol'] for data in data_binance if 'USDT' in data['symbol']]
 
 
 async def on_message(ws, message):
@@ -51,7 +49,10 @@ async def on_message(ws, message):
 
             if notional >= 15000:
                 liquidation_type = "Short" if side == "Buy" else "Long"
-                await message_bybit_binance(-1002304776308, symbol, liquidation_type, f'{notional:.2f}')
+                if symbol in binance_symbol:
+                    await message_bybit_binance(-1002304776308, symbol, liquidation_type, f'{notional:.2f}', price)
+                else:
+                    await message_binance(-1002304776308, symbol, liquidation_type, f'{notional:.2f}', price)
                 logger.info(f"Обработано событие ликвидации: {symbol}, Тип: {liquidation_type}, Сумма: {notional:.2f} USDT")
     except Exception as e:
         logger.error(f"Ошибка при обработке сообщения WebSocket: {e}")
--- user.py
+++ user.py
@@ -3,7 +3,6 @@ from aiogram import Bot
 from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 
-
 config: Config = load_config('.env')
 bot = Bot(token=config.tg_bot.token)
 
@@ -16,7 +15,7 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
-async def message_bybit_binance(tg_id, symbol, liquidation_type, price):
+async def message_bybit_binance(tg_id, symbol, liquidation_type, liquidation, price):
     try:
         sml = {'Short': '🟢', 'Long': '🔴'}
         coinglass = f'https://www.coinglass.com/tv/ru/Bybit_{symbol}'
@@ -26,11 +25,31 @@ async def message_bybit_binance(tg_id, symbol, liquidation_type, price):
         # Отправка сообщения
         await bot.send_message(
             chat_id=tg_id,
-            text=f'{sml[liquidation_type]}<b>{symbol[0:-4]}</b>, сумма ликвидации: {price} USDT\n'
-                 f'<a href=\"{bybit}\">ByBit</a> | <a href=\"{coinglass}\">CoinGlass</a> | <a href=\"{binance}\">Binance</a>',
+            text=f'⚫ByBit: {sml[liquidation_type]} {liquidation_type} <b>{symbol[0:-4]}</b>, liquidation amount: {liquidation} USDT price: {price}\n'
+                 f'<a href=\"{bybit}\">ByBit</a> | <a href=\"{coinglass}\">CoinGlass</a> | <a href=\"{binance}\">Binance</a> ',
             parse_mode=ParseMode.HTML,
             disable_web_page_preview=True
         )
-        logger.info(f"Сообщение отправлено в Telegram для {symbol} с суммой ликвидации {price} USDT.")
+        logger.info(f"Сообщение отправлено в Telegram для {symbol} с суммой ликвидации {liquidation} USDT.")
+    except Exception as e:
+        logger.error(f"Ошибка при отправке сообщения в Telegram: {e}")
+
+
+async def message_binance(tg_id, symbol, liquidation_type, liquidation, price):
+    try:
+        sml = {'Short': '🟢', 'Long': '🔴'}
+        coinglass = f'https://www.coinglass.com/tv/ru/Bybit_{symbol}'
+        bybit = f'https://www.bybit.com/trade/usdt/{symbol}'
+        binance = f'https://www.binance.com/ru/futures/{symbol}'
+
+        # Отправка сообщения
+        await bot.send_message(
+            chat_id=tg_id,
+            text=f'⚫ByBit: {sml[liquidation_type]} {liquidation_type} <b>{symbol[0:-4]}</b>, liquidation amount: {liquidation} USDT price: {price}\n'
+                 f'<a href=\"{bybit}\">ByBit</a> | <a href=\"{coinglass}\">CoinGlass</a>',
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=True
+        )
+        logger.info(f"Сообщение отправлено в Telegram для {symbol} с суммой ликвидации {liquidation} USDT.")
     except Exception as e:
         logger.error(f"Ошибка при отправке сообщения в Telegram: {e}")
