76420ea838455512551cbfe6a77594cb5f3eea0d
havison <dieu2010@mail.ru>
2024-12-23 09:44:24 +1000

    22.12.2024

--- main.py
+++ main.py
@@ -85,14 +85,14 @@ async def on_message(ws, message):
             qty = float(liquidation["size"])
             price = float(liquidation["price"])
             notional = qty * price
-
-            if notional >= 15000 and symbol not in TOP_50_BYBIT:
-                liquidation_type = "Long" if side == "Buy" else "Short"
-                if symbol in binance_symbol:
-                    await message_bybit_binance(-1002304776308, symbol, liquidation_type, f'{notional:.2f}', price)
-                else:
-                    await message_binance(-1002304776308, symbol, liquidation_type, f'{notional:.2f}', price)
-                logger.info(f"Обработано событие ликвидации: {symbol}, Тип: {liquidation_type}, Сумма: {notional:.2f} USDT")
+            if 'USDT' in symbol:
+                if notional >= 15000 and symbol not in TOP_50_BYBIT:
+                    liquidation_type = "Long" if side == "Buy" else "Short"
+                    if symbol in binance_symbol:
+                        await message_bybit_binance(-1002304776308, symbol, liquidation_type, f'{notional:.2f}', price)
+                    else:
+                        await message_binance(-1002304776308, symbol, liquidation_type, f'{notional:.2f}', price)
+                    logger.info(f"Обработано событие ликвидации: {symbol}, Тип: {liquidation_type}, Сумма: {notional:.2f} USDT")
     except Exception as e:
         logger.error(f"Ошибка при обработке сообщения WebSocket: {e}")
 
